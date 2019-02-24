35b1cdc6a90fa6be362eb54e685552ef89dccc7d
Cheng Vue <csalvue@gmail.com>
2019-02-24 17:01:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ function driverNamesWithRevenueOver(drivers, revenue) {
   return drivers.filter(function (driver) {
     if (driver.revenue > revenue) {
       return driver.name;
+    } else {
+      continue;
     }
   });
 }
