3aa38e378f1010eebc981e7ab898d189ccc1824a
Cheng Vue <csalvue@gmail.com>
2019-02-24 17:23:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function driversWithRevenueOver(drivers, revenue) {
 function driverNamesWithRevenueOver(drivers, revenue) {
   return drivers.filter(function (driver) {
     if (driver.revenue > revenue) {
-      return driver;
+      return driver.name;
     }
   });
 }
